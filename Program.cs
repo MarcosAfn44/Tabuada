44218c85a37edd3b4c8472b9aead4b17b8d25390
marcos <marcosantonnioferreiradn@gmail.com>
2022-11-20 19:52:59 -0300

    adiciona a soluçao do exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Tabuada do número:");
+int N= Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine();
+
+int M = 0;
+int produto;
+
+while(M <= 10)
+{
+    produto = N*M;
+    Console.WriteLine($"{N}x{M} = {produto}");
+    M += 1;
+}
